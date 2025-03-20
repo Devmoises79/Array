e8ebf8ca4edf8e4651203350d0f0a5e20ccab4c6
Devmoises79 <devmoisesss@gmail.com>
2025-03-20 17:17:43 -0300

    ListaHobbies

---
+++ array.py
@@ -0,0 +1,12 @@
+hobby_esportes = [] #criando a lista
+
+
+hobby_esportes.append("Futebol") #Adicionando elementos
+hobby_esportes.append("Futsal")
+hobby_esportes.append("Boxe")
+hobby_esportes.append("Basquete")
+hobby_esportes.sort() #Ordenando a lista
+hobby_esportes.pop(1) # Removendo um elemento da lista
+
+
+print(hobby_esportes)
